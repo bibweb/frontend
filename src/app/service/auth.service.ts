2230bcf241cb3d32a9251a80bd851a5be36f1ce2
Stefan Friedli <stefan.friedli@zuehlke.com>
2018-10-22 15:31:11 +0200

    format auth.service tslint

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,63 +1,64 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import * as moment from "moment";
+import {Observable, of} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
+import * as moment from 'moment';
 
-import { LoginUser } from '../model/loginUser';
+import {LoginUser} from '../model/loginUser';
 
-import { environment } from '../../environments/environment';
+import {environment} from '../../environments/environment';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AuthService {
 
   private authUrl = environment.bibwebApiUrl + '/token/generate-token';  // URL to web api
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   login(loginUser: LoginUser): Observable<void> {
-	return this.http.post(this.authUrl, loginUser, httpOptions).pipe(
+    return this.http.post(this.authUrl, loginUser, httpOptions).pipe(
       tap(res => console.log(`logged in user ${loginUser.username}`)),
       catchError(this.handleError<any>('login'))
     );
   }
-  
-  public setSession(token: string, expiresIn: number) {
-	const expiresAt = moment().add(expiresIn, 'second');
-	localStorage.setItem('id_token', token);
-	localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
-  }
-  
-  public isLoggedIn() {
-	if(localStorage.getItem("expires_at") === null) {
-		return false;
-	}
-	return moment().isBefore(this.getExpiration());
-  }
-  
-  public logout(): void {
-	localStorage.removeItem('id_token');
-	localStorage.removeItem('expires_at');
-  }
-  
-  public hasRole(role: string) {
-	if(!this.isLoggedIn()) {
-		return false;
-	}		
-	
-	const token = localStorage.getItem('id_token');
-	const jwtData = token.split('.')[1]
-	const decodedJwtJsonData = window.atob(jwtData)
-	const decodedJwtData = JSON.parse(decodedJwtJsonData)
-	
-	const retVal = decodedJwtData.scopes.toString().includes(role.toString());
 
-	return retVal;
+  public setSession(token: string, expiresIn: number) {
+    const expiresAt = moment().add(expiresIn, 'second');
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+  }
+
+  public isLoggedIn() {
+    if (localStorage.getItem('expires_at') === null) {
+      return false;
+    }
+    return moment().isBefore(this.getExpiration());
+  }
+
+  public logout(): void {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public hasRole(role: string) {
+    if (!this.isLoggedIn()) {
+      return false;
+    }
+
+    const token = localStorage.getItem('id_token');
+    const jwtData = token.split('.')[1];
+    const decodedJwtJsonData = window.atob(jwtData);
+    const decodedJwtData = JSON.parse(decodedJwtJsonData);
+
+    const retVal = decodedJwtData.scopes.toString().includes(role.toString());
+
+    return retVal;
   }
 
   public getUserName(): string {
@@ -68,8 +69,8 @@ export class AuthService {
 
     return decodedJwtData.sub;
   }
-  
-  private handleError<T> (operation = 'operation', result?: T) {
+
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
       // TODO: send the error to remote logging infrastructure
@@ -79,14 +80,14 @@ export class AuthService {
       return of(result as T);
     };
   }
-  
+
   private getExpiration() {
-        const expiration = localStorage.getItem("expires_at");
-        const expiresAt = JSON.parse(expiration);
-        return moment(expiresAt);
-  } 
-  
-  
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+
+
 }
 
 
