bcd567acdea36ea283ff6cfeed6878338d0fe481
Stefan Friedli <stefan.friedli@zuehlke.com>
2018-10-23 11:43:34 +0200

    update nginx start script

--- launch-nginx.sh
+++ launch-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ls -l /usr/share/nginx/html/
-mainFile="$(find /usr/share/nginx/html/ -name main*.js)"
+ls -l /var/www/bibweb-frontend/
+mainFile="$(find /var/www/bibweb-frontend/ -name main*.js)"
 sed -i -E "s|bibwebApiUrl:[ ]*[\"\'][a-zA-Z0-9.\/:]*[\"\']|bibwebApiUrl: \'$API_URL\'|" ${mainFile}
 nginx -g "daemon off;"
